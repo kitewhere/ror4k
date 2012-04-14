0ec75c4ba52d409935d3bf0bf3ab09b74304fa96
kitewhere <kitewhere@hotmail.com>
2012-04-14 20:09:19 +0800

    this is the 1st!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newdemo::Application.config.secret_token = '87d3088386589449930f3514cd5947eddbee84b8e4ca2dc45814496cfbd48887c025f2cadec6c2cdae34d6bba54c41bd55747fecad2f1d1c8051442a68556528'
